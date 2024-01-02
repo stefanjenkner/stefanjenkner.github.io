4f0ee86bf1b5a62f915b057a773bdad0922fc40c
Stefan Jenkner <stefan@jenkner.org>
2024-01-02 13:01:22 +0100

    Switch site generator from Jekyll to Pelican

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source 'https://rubygems.org'
-
-gem "jekyll", "~> 4.3"
-gem "webrick", "~> 1.8"
-
-group :jekyll_plugins do
-    gem 'jekyll-email-protect', '1.1.0'
-    gem 'jekyll-sitemap', '1.4.0'
-end
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+AUTHOR = 'Stefan Jenkner'
+SITENAME = "A developer's notebook"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+)
+
+# Social widget
+SOCIAL = (
+    ("GitHub", "https://github.com/stefanjenkner"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
