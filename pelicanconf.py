0fbf5836cec89e9a7879211f324bf2d2947abf0e
Stefan Jenkner <stefan@jenkner.org>
2024-01-24 20:03:17 +0100

    Updated theme settings and sidebar

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 AUTHOR = 'Stefan Jenkner'
 SITENAME = "A developer's notebook"
+SITETITLE = "Stefan Jenkner"
+SITESUBTITLE = "A developer's notebook"
 SITEURL = ""
 SITELOGO = SITEURL + "/images/profile.png"
 
@@ -16,11 +20,13 @@ TIMEZONE = 'Europe/Berlin'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+MAIN_MENU = True
 
 # Blogroll
 LINKS = (
@@ -30,6 +36,7 @@ LINKS = (
 SOCIAL = (
     ("github", "https://github.com/stefanjenkner"),
     ("mastodon", "https://dresden.network/@stefanjenkner"),
+    ("rss", "/feeds/all.atom.xml"),
 )
 
 DEFAULT_PAGINATION = 10
@@ -38,5 +45,10 @@ DEFAULT_PAGINATION = 10
 # RELATIVE_URLS = True
 
 THEME = 'themes/Flex'
+THEME_COLOR = 'dark'
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
 
 GITHUB_CORNER_URL = "https://github.com/stefanjenkner/stefanjenkner.github.io"
+
+COPYRIGHT_YEAR = datetime.now().year
